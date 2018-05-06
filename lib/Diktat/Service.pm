4002dd0ce717ab47381b442a54e7efac194a323e
Loïc TROCHET <losyme@protonmail.com>
2018-05-06 18:05:54 +0200

    Ajout du µs 'Diktat'

---
+++ lib/Diktat/Service.pm
@@ -0,0 +1,31 @@
+#######
+##   ___ _______ _  ___ ___       _______  _______
+##  / _ `/ __/  ' \/ -_) _ \  _  / __/ _ \/ __/ -_)
+##  \_,_/_/ /_/_/_/\__/_//_/ (_) \__/\___/_/  \__/
+##
+####### Ecosystème basé sur les microservices ##################### (c) 2018 losyme ####### @(°_°)@
+
+package Diktat::Service;
+
+#md_# Diktat::Service
+#md_
+
+use Exclus::Exclus;
+use Moo;
+use namespace::clean;
+
+extends qw(Obscur::Runner::Service);
+
+#md_## Les attributs
+#md_
+
+###----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----###
+has '+name'        => (default => sub { 'Diktat' });
+has '+description' => (default => sub { 'Le µs chargé de générer des évènements' });
+###----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----###
+
+#md_## Les méthodes
+#md_
+
+1;
+__END__
