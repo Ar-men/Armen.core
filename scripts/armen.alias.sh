2c997150ce9b21d2fa723cd7ca856d2890e4492a
Loïc TROCHET <losyme@protonmail.com>
2018-05-06 17:50:00 +0200

    Ajout du script pour les alias

---
+++ scripts/armen.alias.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#######
+##   ___ _______ _  ___ ___       _______  _______
+##  / _ `/ __/  ' \/ -_) _ \  _  / __/ _ \/ __/ -_)
+##  \_,_/_/ /_/_/_/\__/_//_/ (_) \__/\___/_/  \__/
+##
+####### Ecosystème basé sur les microservices ##################### (c) 2018 losyme ####### @(°_°)@
+
+alias aav='armen available'
+alias ade='armen decrypt'
+alias aen='armen encrypt'
+alias ato='armen stop'
+alias are='armen registered'
+alias ata='armen start'
+alias aex='armen execute'
+
+####### END
